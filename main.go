37dcd24e852971b13e5d878a088ff7c520c6eb78
Kai Jun Tam <thek3@thek3-MacBook-Pro.local>
2024-03-03 22:02:09 +0800

    implement get record from db by record id

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -18,6 +18,8 @@ import (
 
 var c *dynamodb.Client
 
+type DynamoAttribute map[string]types.AttributeValue
+
 func main() {
 	if c == nil {
 		var err error
@@ -27,61 +29,9 @@ func main() {
 		}
 	}
 
-	http.HandleFunc("GET /api/v1/records", handleGetRecord)
+	http.HandleFunc("GET /api/v1/records/{recordId}", handleGetRecord)
 	http.HandleFunc("POST /api/v1/records", handlePostRecord)
 	http.ListenAndServe(":8080", nil)
-}
-
-func handleGetRecord(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("im in")
-	fmt.Fprintf(w, "print from insiders")
-}
-
-func handlePostRecord(w http.ResponseWriter, r *http.Request) {
-
-	var record model.Record
-	err := json.NewDecoder(r.Body).Decode(&record)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	err = putRecord(c, "Records", record)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-}
-
-func unsafeToAttrValue(in interface{}) types.AttributeValue {
-	val, err := attributevalue.Marshal(in)
-	if err != nil {
-		log.Fatalf("could not marshal value `%v` with error: %v", in, err)
-	}
-
-	return val
-}
-
-func putRecord(c *dynamodb.Client, tableName string, record model.Record) (err error) {
-
-	dynoItem := map[string]types.AttributeValue{
-		"RecordId":   unsafeToAttrValue(record.RecordId),
-		"CaseId":     unsafeToAttrValue(record.CaseId),
-		"RecordName": unsafeToAttrValue(record.RecordName),
-		"MetricUnit": unsafeToAttrValue(record.MetricUnit),
-		"Details":    unsafeToAttrValue(record.Details),
-	}
-	_, err = c.PutItem(context.TODO(), &dynamodb.PutItemInput{
-		TableName: aws.String(tableName), Item: dynoItem,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func newclient(profile string) (*dynamodb.Client, error) {
@@ -104,4 +54,78 @@ func newclient(profile string) (*dynamodb.Client, error) {
 
 	c := dynamodb.NewFromConfig(cfg)
 	return c, nil
+}
+
+func handleGetRecord(w http.ResponseWriter, r *http.Request) {
+	recordId, _ := strconv.Atoi(r.PathValue("recordId"))
+	dynoItem := DynamoAttribute{
+		"RecordId": unsafeToAttrValue(recordId),
+		// "CaseId": unsafeToAttrValue(r.PathValue("caseId")),
+	}
+
+	records, err := getRecords(c, "Records", dynoItem)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var record model.Record
+	attributevalue.UnmarshalMap(records, &record)
+	json.NewEncoder(w).Encode(record)
+}
+
+func handlePostRecord(w http.ResponseWriter, r *http.Request) {
+
+	var record model.Record
+	err := json.NewDecoder(r.Body).Decode(&record)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = putRecord(c, "Records", record)
+
+	if err != nil {
+		print("Error here: ", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+}
+
+func putRecord(c *dynamodb.Client, tableName string, record model.Record) (err error) {
+
+	dynoItem := DynamoAttribute{
+		"RecordId":   unsafeToAttrValue(record.RecordId),
+		"CaseId":     unsafeToAttrValue(record.CaseId),
+		"RecordName": unsafeToAttrValue(record.RecordName),
+		"MetricUnit": unsafeToAttrValue(record.MetricUnit),
+		"Details":    unsafeToAttrValue(record.Details),
+	}
+	_, err = c.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String(tableName), Item: dynoItem,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getRecords(c *dynamodb.Client, tableName string, key DynamoAttribute) (item DynamoAttribute, err error) {
+	resp, err := c.GetItem(context.TODO(), &dynamodb.GetItemInput{Key: key, TableName: aws.String(tableName)})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Item, nil
+}
+
+func unsafeToAttrValue(in interface{}) types.AttributeValue {
+	val, err := attributevalue.Marshal(in)
+	if err != nil {
+		log.Fatalf("could not marshal value `%v` with error: %v", in, err)
+	}
+
+	return val
 }
